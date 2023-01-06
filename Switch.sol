8be175ab284f4a9b56b48b84542c3522c093029f
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2023-01-07 01:24:49 +0400

    add Switch contract

---
+++ Switch.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract Switch {
+    address public owner;
+    address public recipient;
+    uint public lastActive;
+
+    constructor(address _recipient) payable {
+        recipient = _recipient;
+        owner = msg.sender;
+        lastActive = block.timestamp;
+    }
+
+    function withdraw() external {
+        require(lastActive + 52 weeks < block.timestamp);
+        (bool success, ) = recipient.call{ value: address(this).balance }("");
+        require(success, "Failed to Withdraw Funds");
+    }
+
+    function ping() external {
+        require(owner == msg.sender);
+        lastActive = block.timestamp;
+    }
+}
